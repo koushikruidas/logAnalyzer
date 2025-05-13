4d844927269a59eb0e244c9aecebd438316535e5
Koushik Ruidas <koushikruidas@gmail.com>
2025-05-13 13:50:28 +0530

    added SASL_SSL for kafka broker communication.

--- src/main/java/com/poinciana/loganalyzer/config/KafkaConsumerConfig.java
+++ src/main/java/com/poinciana/loganalyzer/config/KafkaConsumerConfig.java
@@ -22,7 +22,7 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Value("${spring.kafka.consumer.group-id}")
+    @Value("${spring.kafka.consumer.groupId}")
     private String groupId;
     @Value(("${spring.kafka.consumer.pollType}"))
     private String pollType;
@@ -33,6 +33,19 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.concurrency}")
     private int concurrency;
 
+    // SSL configuration properties
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${spring.kafka.properties.sasl.username}")
+    private String saslUsername;
+
+    @Value("${spring.kafka.properties.sasl.password}")
+    private String saslPassword;
+
+//    @Value("${spring.kafka.properties.sasl.mechanism}")
+//    private String saslMechanism;
+
     @Bean
     public Map<String, Object> kafkaListerConfig(){
         log.info("Inside kafkaListenerConfig()");
@@ -40,13 +53,28 @@ public class KafkaConsumerConfig {
         log.info("PollType: "+pollType);
 
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBroker );
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBroker);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, pollType);
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxRecods);
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        // SSL Configurations for the consumer
+        properties.put("security.protocol", securityProtocol);
+        properties.put("ssl.keystore.location", System.getenv("SPRING_KAFKA_PROPERTIES_SSL_KEYSTORE_LOCATION"));
+        properties.put("ssl.keystore.password", System.getenv("SPRING_KAFKA_PROPERTIES_SSL_KEYSTORE_PASSWORD"));
+        properties.put("ssl.key.password", System.getenv("SPRING_KAFKA_PROPERTIES_SSL_KEY_PASSWORD"));
+        // Using default truststore (cacerts)
+
+        // SASL Configurations
+        properties.put("sasl.mechanism", "PLAIN"); // Or use SCRAM-SHA-256 if your broker supports it
+        properties.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                        "username=\""+ saslUsername +"\" password=\"" + saslPassword + "\";");
+
+
         return properties;
     }
 
--- src/main/java/com/poinciana/loganalyzer/config/KafkaTopicResolver.java
+++ src/main/java/com/poinciana/loganalyzer/config/KafkaTopicResolver.java
@@ -6,19 +6,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class KafkaTopicResolver {
     @Value("${ORG_ID:logs}")
     private String orgId;
+    @Value("${spring.kafka.properties.sasl.username}")
+    private String saslUsername;
+
+    @Value("${spring.kafka.properties.sasl.password}")
+    private String saslPassword;
+
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
 
     @Autowired
     private KafkaAdmin kafkaAdmin;
 
     public List<String> getTopics() {
-        try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
+        Map<String, Object> adminProps = new HashMap<>(kafkaAdmin.getConfigurationProperties());
+
+        adminProps.put("security.protocol", securityProtocol);
+        adminProps.put("sasl.mechanism", "PLAIN");
+
+        // Inject JAAS config inline
+        adminProps.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                        "username=\"" + saslUsername + "\" password=\"" + saslPassword + "\";");
+        try (AdminClient adminClient = AdminClient.create(adminProps)) {
             return adminClient.listTopics().names().get().stream()
                     .filter(topic -> topic.startsWith(orgId + "_"))
                     .collect(Collectors.toList());
